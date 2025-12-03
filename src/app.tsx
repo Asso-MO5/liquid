2794de336376ec9161e93ba231e1fd441b8a6db1
Kazerlelutin <b@bouteiller.contact>
2025-12-03 22:52:33 +0100

    meta

--- src/app.tsx
+++ src/app.tsx
@@ -20,6 +20,9 @@ export default function App() {
           <Title>Le musée du jeu vidéo</Title>
           <Meta name="description" content="Le musée du jeu vidéo" />
           <Meta name="keywords" content="musée, jeu vidéo, jeux, jeux vidéo, jeux de société, jeux de sociétés, jeux de sociétés en ligne, jeux de sociétés en ligne gratuit, jeux de sociétés en ligne gratuitement, jeux de sociétés en ligne gratuitement sans inscription, jeux de sociétés en ligne gratuitement sans inscription, jeux de sociétés en ligne gratuitement sans inscription, jeux de sociétés en ligne gratuitement sans inscription" />
+          <Meta name="og:image" content="/og.png" />
+          <Meta name="og:description" content="Le musée du jeu vidéo" />
+          <Meta name="og:title" content="Le musée du jeu vidéo" />
           <Meta name="viewport" content="width=device-width, initial-scale=1" />
           <Suspense fallback={<div class="flex items-center justify-center p-3 h-screen"><Loader /></div>}>
             <Layout>
