77f585de9ff07b6d0a771c5c70226071124694d3
Cyrille Jouineau <cjouineau@yahoo.com>
2025-12-17 11:59:37 +0100

    better alternative text for MO5 logos
    Suppression de "Logo de" dans le header et le footer, et suppression totale dans le texte d'introduction où il est superflu dans le contexte.

--- src/features/asso/asso.tsx
+++ src/features/asso/asso.tsx
@@ -2,12 +2,10 @@ import { langCtrl } from "../lang-selector/lang.ctrl"
 
 const txt = {
   fr: {
-    alt: 'Logo de l\'association MO5',
     speach: 'Après plus de vingt ans de préservation, de restauration et de transmission du patrimoine vidéoludique, l’association MO5 franchit une étape historique : la première phase de l’ouverture du Musée du Jeu Vidéo. Ce lieu, unique en France, sera entièrement consacré à l’histoire, à la culture et à la mémoire du jeu vidéo, de ses créateurs et de son influence sur la société contemporaine.',
     discoverAssociation: 'Découvrez l\'association MO5',
   },
   en: {
-    alt: 'Logo of the MO5 association',
     speach: 'After more than twenty years of preservation, restoration and transmission of the video game heritage, the MO5 association takes a historic step: the first phase of the opening of the Video Game Museum. This place, unique in France, will be entirely dedicated to the history, culture and memory of video games, its creators and its influence on contemporary society.',
     discoverAssociation: 'Discover the MO5 association',
   }
@@ -17,7 +15,7 @@ export const Asso = () => {
   return (
     <div class="flex flex-col gap-5 max-w-2xl mx-auto">
       <div class="flex flex-col md:flex-row gap-5 h-full justify-center items-center ">
-        <img src="/logo_blue.webp" alt={txt[lang() as keyof typeof txt].alt}
+        <img src="/logo_blue.webp" alt=""
           loading="lazy"
           width={100}
           height={87}
