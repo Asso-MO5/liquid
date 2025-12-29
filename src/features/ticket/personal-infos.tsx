4886f4ee9541e35b57e6363385f189315d62c959
Kazerlelutin <b@bouteiller.contact>
2025-12-29 17:41:55 +0100

    désactivation de l'autocomplete sur le mail

--- src/features/ticket/personal-infos.tsx
+++ src/features/ticket/personal-infos.tsx
@@ -1,9 +1,21 @@
 import { ticketTxt } from "./ticket.txt"
 import { setTicketStore, ticketStore } from "./ticket.store"
 import { translate } from "~/utils/translate";
+import { onMount } from "solid-js";
 
 export const PersonalInfos = () => {
   const { t } = translate(ticketTxt)
+
+  let emailInputRef: HTMLInputElement | undefined;
+
+  onMount(() => {
+    // Retirer readonly au focus pour permettre la saisie
+    emailInputRef?.addEventListener('focus', () => {
+      if (emailInputRef) {
+        emailInputRef.removeAttribute('readonly');
+      }
+    });
+  });
 
   return (
     <div class="flex flex-col gap-2">
@@ -27,12 +39,38 @@ export const PersonalInfos = () => {
       </div>
       <div class="flex flex-col gap-1">
         <label for="email" class="text-primary">{t.email}</label>
+        {/* Champ factice pour tromper l'autocomplétion */}
         <input
+          type="email"
+          name="email_fake"
+          autocomplete="off"
+          tabindex="-1"
+          style={{ position: 'absolute', left: '-9999px', opacity: 0, 'pointer-events': 'none' }}
+          aria-hidden="true"
+        />
+        <input
+          ref={emailInputRef}
           aria-label={t.email}
           aria-required="true"
           required
-          auto-complete="email"
-          id="email" type="email" class="bg-white/10 text-text" value={ticketStore.email} onInput={(e) => setTicketStore('email', e.currentTarget.value)} />
+          readonly
+          autocomplete="new-password"
+          data-form-type="other"
+          data-lpignore="true"
+          data-1p-ignore="true"
+          data-bwignore="true"
+          name="email_input"
+          id="email"
+          type="email"
+          class="bg-white/10 text-text"
+          value={ticketStore.email}
+          onInput={(e) => {
+            if (emailInputRef) {
+              emailInputRef.removeAttribute('readonly');
+            }
+            setTicketStore('email', e.currentTarget.value);
+          }}
+        />
       </div>
       <p class="text-xs text-amber-500 italic">{t.warningEmail}</p>
     </div>
