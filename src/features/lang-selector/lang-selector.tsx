865b5e90a0c6300d664c0ebdf23096b47df9e65c
Cyrille Jouineau <cjouineau@yahoo.com>
2026-01-02 22:58:31 +0100

    Reload after switching language

--- src/features/lang-selector/lang-selector.tsx
+++ src/features/lang-selector/lang-selector.tsx
@@ -1,20 +1,18 @@
-import { useLocation, useNavigate, useParams } from '@solidjs/router'
+import { useLocation, useParams } from '@solidjs/router'
 import { LANGS, SELECT } from './lang-selector.const'
 import { createSignal, For, onMount, Show } from 'solid-js'
 
 export const LangSelector = () => {
   const { lang: langParams = 'fr' } = useParams()
-  const navigate = useNavigate()
   const path = useLocation()
   const [selectedLang, setSelectedLang] = createSignal('')
 
   const changeLang = (lang: 'fr' | 'en') => {
     document.documentElement.setAttribute('lang', lang)
-
-    navigate(
-      path.pathname.replace(selectedLang() || '', lang) + window.location.search
-    )
+    const newPath = path.pathname.replace(selectedLang() || '', lang) + window.location.search
     setSelectedLang(lang)
+    window.history.pushState({}, '', newPath)
+    window.location.reload()
   }
   onMount(() => {
     setSelectedLang(langParams)
