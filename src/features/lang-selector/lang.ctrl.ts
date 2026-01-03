9c3cbb3f74dd55bd8f9120e564b4a33997c664ea
Kazerlelutin <79605267+kazerlelutin@users.noreply.github.com>
2026-01-03 12:11:32 +0100

    sécurisation langue (#148)
    Ne plus permettre une autre langue que celles désignées.

--- src/features/lang-selector/lang.ctrl.ts
+++ src/features/lang-selector/lang.ctrl.ts
@@ -3,6 +3,7 @@ import { useParams } from "@solidjs/router"
 export const langCtrl = () => {
   const params = useParams()
   const _lang = () => params?.lang as 'fr' | 'en'
-  const lang = () => !_lang?.()?.match?.(/fr|en/) ? 'fr' : _lang()
+  const lang = () => !['fr', 'en'].includes(_lang?.()) ? 'fr' : _lang()
+
   return lang
 }
