9c12bcb1778f1e222cdfede19a9f28bf46bf383e
Cyrille Jouineau <cjouineau@yahoo.com>
2026-01-04 10:26:08 +0100

    A11Y for theme selection

--- src/features/dark-mode/dark-mode.tsx
+++ src/features/dark-mode/dark-mode.tsx
@@ -1,11 +1,14 @@
 import { darkModeCtrl } from "./dark-mode.ctrl"
 import { Show } from "solid-js"
+import { translate } from "~/utils/translate"
+import { darkModeTxt } from "./dark-mode.txt"
 
 export const DarkMode = () => {
   const darkMode = darkModeCtrl()
+  const { t } = translate(darkModeTxt)
+
   return (
-    <div onClick={darkMode.toggleDarkMode} class="cursor-pointer">
-      <div class="w-6 h-6 rounded-full" data-dark={darkMode.isDarkMode()}>
+    <button type="button" onClick={darkMode.toggleDarkMode} class="cursor-pointer border-transparent hover:bg-emerald-500/20" data-dark={darkMode.isDarkMode()} title={darkMode.isDarkMode() === 'dark' ? t().toLight : t().toDark}>
         <Show when={darkMode.isDarkMode() === 'light'}>
           <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-amber-600"><circle cx="12" cy="12" r="5" /><line x1="12" x2="12" y1="1" y2="3" /><line x1="12" x2="12" y1="21" y2="23" /><line x1="4.22" x2="5.64" y1="4.22" y2="5.64" /><line x1="18.36" x2="19.78" y1="18.36" y2="19.78" /><line x1="1" x2="3" y1="12" y2="12" /><line x1="21" x2="23" y1="12" y2="12" /><line x1="12" x2="14" y1="18" y2="18" /><line x1="12" x2="14" y1="6" y2="6" /></svg>
         </Show>
@@ -14,7 +17,6 @@ export const DarkMode = () => {
 
             width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="stroke-amber-300"><path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z" /></svg>
         </Show>
-      </div>
-    </div>
+    </button>
   )
 }
---
+++ src/features/dark-mode/dark-mode.txt.ts
@@ -0,0 +1,11 @@
+export const darkModeTxt = {
+  fr: {
+    toDark: 'Utliser le thème sombre',
+    toLight: 'Utliser le thème clair',
+  },
+  en: {
+    toDark: 'Apply dark theme',
+    toLight: 'Apply light theme',
+  }
+}
+
