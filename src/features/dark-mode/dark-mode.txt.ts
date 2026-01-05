2b16a7f8e8787976f2dd9a4ee8958faf0a282cf5
Cyrille Jouineau <cjouineau@yahoo.com>
2026-01-05 20:35:53 +0100

    A11Y for theme selection (#195)
    * Gestion de date de fin coté back
    
    * A11Y for theme selection
    
    * typo
    
    ---------
    
    Co-authored-by: Kazerlelutin <b@bouteiller.contact>

---
+++ src/features/dark-mode/dark-mode.txt.ts
@@ -0,0 +1,11 @@
+export const darkModeTxt = {
+  fr: {
+    toDark: 'Utiliser le thème sombre',
+    toLight: 'Utiliser le thème clair',
+  },
+  en: {
+    toDark: 'Apply dark theme',
+    toLight: 'Apply light theme',
+  }
+}
+
