0d562b56bb43550b30f37ff8bd8e21bb34e44f73
Cyrille Jouineau <cjouineau@yahoo.com>
2026-01-11 09:43:52 +0100

    a11y gift codes

--- src/features/gift-codes/gift-codes.ctrl.ts
+++ src/features/gift-codes/gift-codes.ctrl.ts
@@ -10,17 +10,36 @@ export const giftCodesCtrl = () => {
       newCodes.push(value);
     }
     setTicketStore('gift_codes', newCodes);
+
+    // when newCodes changes, focus is lost (I suppose it is the rerender) so we place it back to the next focusable element
+    if (document) {
+      document.getElementById(`gift-code-remove-${index}`)?.focus()
+    }
   }
 
   const addGiftCode = () => {
     if (ticketStore.gift_codes.length < ticketStore.tickets.length) {
       setTicketStore('gift_codes', [...ticketStore.gift_codes, '']);
+      
+      // set focus to the newly created input
+      if (document) {
+        const giftCodesList = document.getElementById('gift-codes-list')
+        const newestInput = giftCodesList?.lastElementChild?.querySelector('input')
+        newestInput?.focus()
+      }
     }
   }
 
   const removeGiftCode = (index: number) => {
     const newCodes = ticketStore.gift_codes.filter((_, i) => i !== index);
     setTicketStore('gift_codes', newCodes);
+    
+    // set focus to the last remove button (to ease removing all codes) if any, else back to the add button
+    if (document) {
+      const previousRemoveButton = document.getElementById(`gift-code-remove-${index - 1}`)
+      const addButton = document.getElementById('gift-codes-add')
+      previousRemoveButton ? previousRemoveButton.focus() : addButton?.focus()
+    }
   }
 
   return {
--- src/features/gift-codes/gift-codes.tsx
+++ src/features/gift-codes/gift-codes.tsx
@@ -6,35 +6,40 @@ import { giftCodesTxt } from "./gift-codes.txt"
 
 export const GiftCodes = () => {
   const { t } = translate(giftCodesTxt)
-  const giftCodesCtrlStore = giftCodesCtrl();
+  const giftCodesCtrlStore = giftCodesCtrl()
 
   return (
     <div class="flex flex-col gap-4 mt-4">
-      <For each={ticketStore.gift_codes}>
-        {(code, index) => (
-          <div class="flex gap-2">
-            <input
-              type="text"
-              class="bg-white/10 text-primary border border-primary rounded-md p-2 flex-1"
-              placeholder={t().placeholder}
-              value={code}
-              onInput={(e) => {
-                giftCodesCtrlStore.setGiftCodes(e.currentTarget.value, index())
-              }}
-            />
-            {ticketStore.gift_codes.length > 0 && (
-              <button
-                class="btn"
-                onClick={() => giftCodesCtrlStore.removeGiftCode(index())}
-              >
-                ×
-              </button>
-            )}
-          </div>
-        )}
-      </For>
+      <div id="gift-codes-list" class="flex flex-col gap-4">
+        <For each={ticketStore.gift_codes}>
+          {(code, index) => (
+            <div class="flex gap-2">
+              <input
+                id={`gift-code-input-${index()}`}
+                type="text"
+                class="bg-white/10 text-primary border border-primary rounded-md p-2 flex-1"
+                placeholder={t().placeholder}
+                title={t().placeholder}
+                value={code}
+                onChange={(e) => giftCodesCtrlStore.setGiftCodes(e.currentTarget.value, index())}
+              />
+              {ticketStore.gift_codes.length > 0 && (
+                <button
+                  id={`gift-code-remove-${index()}`}
+                  class="btn"
+                  onClick={() => giftCodesCtrlStore.removeGiftCode(index())}
+                  aria-label={t().remove}
+                >
+                  ×
+                </button>
+              )}
+            </div>
+          )}
+        </For>
+      </div>
+      
       <Show when={ticketStore.gift_codes.length < ticketStore.tickets.filter(ticket => ticket.amount > 0).length}>
-        <button class="btn" onClick={() => giftCodesCtrlStore.addGiftCode()}>
+        <button class="btn" onClick={() => giftCodesCtrlStore.addGiftCode()} id="gift-codes-add" aria-controls="gift-codes-list">
           {t().add}
         </button>
       </Show>
