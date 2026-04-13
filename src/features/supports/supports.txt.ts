dd5dd1bf660987ab83a1902d19abc67f6727e298
Cyrille Jouineau <cjouineau@yahoo.com>
2026-04-13 19:47:25 +0200

    fix lang issue and add links to supports
    Fix #1496

--- src/features/supports/supports.txt.ts
+++ src/features/supports/supports.txt.ts
@@ -1,9 +1,12 @@
 export const texts = {
   fr: {
-    title: 'Ils nous soutiennent',
+    title: "Ils nous soutiennent",
+    readMore: "En savoir plus",
+    ariaLabel: "En savoir plus sur ",
   },
   en: {
-    title: 'They support us',
-  }
+    title: "They support us",
+    readMore: "Find out more",
+    ariaLabel: "Find out more about ",
+  },
 }
-
